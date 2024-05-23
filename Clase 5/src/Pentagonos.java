cdec5c2f3cd42ccc3c1228608238ec2db518a880
alejandro4-admin <alejandro.gutierrez01@epn.edu.ec>
2024-05-23 08:26:37 -0500

    Segundo Cambio

---
+++ Clase 5/src/Pentagonos.java
@@ -0,0 +1,19 @@
+public class Pentagonos extends FigurasGeometricas {
+    double tamanioLados;
+    double apotema;
+
+    public Pentagonos() {
+    }
+
+    public Pentagonos(String nombre, int nlados, boolean regularidad, double tamanioLados, double apotema) {
+        super(nombre, nlados, regularidad);
+        this.tamanioLados = tamanioLados;
+        this.apotema = apotema;
+    }
+    public double calcularArea(){
+        return apotema * 5 * tamanioLados;
+    }
+    public double calcularPerimetro(){
+        return tamanioLados * 5;
+    }
+}
